36d9ee5efb67259e28a041b0fefc3dc7b6cf9569
bradencwatkins <bradencwatkins@gmail.com>
2024-11-07 12:47:37 -0700

    work saved to switch computers - I got to adding each page but it doesnt work yet

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,63 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Vote } from './vote/vote';
+import { Results } from './results/results';
+import { About } from './about/about';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
-}
+    return (
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              MarryaBook<sup>&reg;</sup>
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Home
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='vote'>
+                  Vote
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='results'>
+                  Results
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='about'>
+                  About
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
+  
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/vote' element={<Vote />} />
+            <Route path='/results' element={<Results />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+  
+        <footer className='bg-dark text-white-50'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Author Name(s)</span>
+            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+      </BrowserRouter>
+    );
+  }
---
+++ src/results/results.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Results() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>results displayed here</div>
+    </main>
+  );
+}
