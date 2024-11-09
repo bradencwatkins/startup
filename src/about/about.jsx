4d67c7ec1e540a3e2fce6feed19498dd7197fccb
Braden Watkins <bradencwatkins@gmail.com>
2024-11-09 16:25:28 -0700

    good progress yes

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './about.css';
 
 export function About() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
+    <main className='container-fluid text-center'>
       <div>about displayed here</div>
     </main>
   );
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 export function Login() {
   return (
   
-    <main className="container-fluid bg-secondary text-center">
+    <main className="container-fluid text-center">
       <div>
         <h1 className="custom-text">Login to MarryaBook</h1>
         <form method="get" action="play.html">
           <div class="input-group mb-3">
-            <span class="input-group-text">@</span>
+            <span class="input-group-text">Email</span>
             <input class="form-control" type="text" placeholder="your@email.com" />
           </div>
           <div class="input-group mb-3">
-            <span class="input-group-text">🔒</span>
+            <span class="input-group-text">Password</span>
             <input class="form-control" type="password" placeholder="password" />
           </div>
-          <button type="submit" class="btn btn-primary">Login</button>
-          <button type="submit" class="btn btn-secondary">Create</button>
+          <button type="submit" class="btn btn-primary login">Login</button>
+          <button type="submit" class="btn btn-primary login">Create</button>
         </form>
       </div>
     </main>
