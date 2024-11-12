a280da20eb6a1b92694b94e11d8b23b7a4247dc6
Braden Watkins <bradencwatkins@gmail.com>
2024-11-12 13:30:10 -0700

    commit13

--- src/vote/vote.jsx
+++ src/vote/vote.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export function Vote() {
   return (
-    <main className='container-fluid text-center'>
-      <div>vote displayed here</div>
-    </main>
+    <main>
+            <div class="swipey">
+                <h2>Ellen Johnson</h2>
+                <img src="ellen.jpg" width="300" height="400"></img>
+                <p>Age: 21  Height: 5'2"</p>
+                <button type="button" class="btn btn-success">
+                    <i class="bi bi-check"></i> 
+                </button>
+                <button type="button" class="btn btn-danger">
+                    <i class="bi bi-x"></i>
+                </button>
+            </div>
+        </main>
   );
 }
