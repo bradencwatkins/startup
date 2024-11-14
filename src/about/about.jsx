c8ee7013985449f270f9561f0519e215b427efad
bradencwatkins <bradencwatkins@gmail.com>
2024-11-13 18:38:26 -0700

    about page

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -6,14 +6,14 @@ export function About() {
     <main className='container-fluid text-center'>
       <h2>About MarryaBook</h2>
         <div class="bordered-box">
-          <p>Are you a returned missionary who is sick of not being able to find the one?</p>
-          <p>Were you used to using Area Book on your mission and wish there was a dating equivelant?</p>
-          <p>MarryaBook is for you!! (name subject to change)</p>    
-          <p>The website works in a way similar to other dating sites/apps, but with a key improevment...</p>
-          <p>MarryaBook uses your friends and friends of friends to find your future spouse. After linking your social media, Marryabook will give you suggestions on who to go on dates with.</p>
+          <p>Do you want to know what the best movie or book is?</p>
+          <p>Are you tired of not knowing what to watch or what to read?</p>
+          <p class="bold-text">MarryaBook is for you!!</p>    
+          <p>This website allows you to voice your opinion on what the best media is on the market today!</p>
+          <p>By logging in and going to the vote section, you can let others know what you think is the best movies to watch and books to read.</p>
         </div>
         <div class="pic">
-          <img src="marryabook2.png" width="300" height="300"></img>
+          <img src="logo.png" width="300" height="300" class="imgBorder"></img>
         </div>
     </main>
         
